77b7f72a1429c9c90c2f51afd50266bd2630230f
msramalho <up201403027@fe.up.pt>
2017-02-26 18:23:50 +0000

    setup file for the project
    Maybe change the name later for pypi

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='masterParserPython',
+      version='0.1',
+      description='Parse input files (inputFile.txt) into python variables by specifying the layout of the variables (parserLayout.txt)',
+      url='https://github.com/msramalho/masterParserPython',
+      author='msramalho',
+      license='MIT',
+      packages=['masterParserPython'],
+      zip_safe=False)
