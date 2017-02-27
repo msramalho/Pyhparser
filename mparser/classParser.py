b8e9036ac28636cd44ff17168526f017e405b0e6
msramalho <up201403027@fe.up.pt>
2017-02-27 23:39:45 +0000

    created new helper module for classes parsing

---
+++ mparser/classParser.py
@@ -0,0 +1,13 @@
+class classParser:#stores and manages a list of the classes used by the parser, also creates instances of them
+    def __init__(self, classList):
+        self.classList = classList
+        self.classNames = list(map(lambda c: c.__name__, classList))
+    def hasClass(self, className):
+        return className in self.classNames
+    def getClass(self, className):
+        for c in self.classList:
+            if c.__name__ == className:
+                return c
+        return None
+    def initClass(self, className, parameters):#dict
+        return self.getClass(className)(**parameters)
