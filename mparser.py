0b83b62fd4696ea52be13fb9965e96e23ad3763c
msramalho <up201403027@fe.up.pt>
2017-02-28 00:25:40 +0000

    Refactor - read description
    Combined all the dependencies into one file, mparser.py
    compartimentalized the examples in another folder
    removed package structure

--- mparser.py
+++ mparser.py
@@ -1,6 +1,21 @@
 from sys import argv
 import re, tokenize, io, getopt
-from classParser import *
+
+class classParser:#stores and manages a list of the classes used by the parser, also creates instances of them
+    def __init__(self, classList):
+        self.classList = classList
+        self.classNames = list(map(lambda c: c.__name__, classList))
+    def hasClass(self, className):
+        return className in self.classNames
+    def getClass(self, className):
+        for c in self.classList:
+            if c.__name__ == className:
+                return c
+        return None
+    def initClass(self, className, parameters):#dict
+        return self.getClass(className)(**parameters) if self.hasClass(className) else False
+
+
 #TODO: include set and frozenset, see tuple implementation in (varName, tuple, len) format
 
 def displayUnavailableVariables():
--- mparser/__init__.py
+++
@@ -1,3 +0,0 @@
-import mparser
-
-mparser.displayUsage()
