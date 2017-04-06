9ffce70060b489861b8b7cfa01c85f2c9fec0e20
msramalho <up201403027@fe.up.pt>
2017-04-06 01:01:14 +0100

    created second test for container variables

---
+++ tests/test_02.py
@@ -0,0 +1,11 @@
+from testUtils import *
+
+title = "Test list creation"
+inputVar = "      5        each word is another word    "
+parserVar = "(size, int) [myList, list, {size}, (str)]"
+fullTest(inputVar, parserVar, title, 2)
+
+title = "Test list creation"
+inputVar = "      5        each word is another word    "
+parserVar = "(size, int) [myList, list, {size}, (str)]"
+fullTest(inputVar, parserVar, title, 2)
