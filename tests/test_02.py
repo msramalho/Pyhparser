491c7e4e0fd8173f8083fbddf73c7872fa15e7fd
msramalho <up201403027@fe.up.pt>
2017-04-06 13:32:26 +0100

    improved second tests

--- tests/test_02.py
+++ tests/test_02.py
@@ -5,7 +5,7 @@ inputVar = "      5        each word is another word    "
 parserVar = "(size, int) [myList, list, {size}, (str)]"
 fullTest(inputVar, parserVar, title, 2)
 
-title = "Test list creation"
-inputVar = "      5        each word is another word    "
-parserVar = "(size, int) [myList, list, {size}, (str)]"
-fullTest(inputVar, parserVar, title, 2)
+title = "Test list creation variable size"
+inputVar = "      3          1        2         4           first now second and finally the last    "
+parserVar = "(size, int) [sizesList, list, {size}, (int)] [sentences, list, {size}, (str, {sizesList;sentences})]"
+fullTest(inputVar, parserVar, title, 3)
