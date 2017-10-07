b36b1a5bf10169b7949c3c54b0d9f55c77a42694
msramalho <up201403027@fe.up.pt>
2017-10-07 20:11:31 +0100

    continued grammar structure

--- pyhparser/grammar.py
+++ pyhparser/grammar.py
@@ -1,31 +1,56 @@
 from pyparsing import *
 
 #elementary operators
-type = Word("int") | Word("long") | Word("float") | Word("bool") | Word("complex") | Word("str")
+pType = Or(Word("int") | Word("long") | Word("float") | Word("bool") | Word("complex")) #primitive types
+sType = Or(Word("str") | Word("bytes") | Word("bytearray")) #sequence types
+cType = Or(Word("list") | Word("tuple") | Word("dict") | Word("set") | Word("frozenset")) #containerTypes
 
-variableName = Word(alphanums + "_") # example n camelCase n1 with_underscore
+immutable = Or(pType | Word("str") | Word("bytes") | Word("tuple") | Word("frozenset"))
 
-c = Suppress(",")	# comma
-op = Suppress("(")	# open parentheses
-cp = Suppress(")")	# close parentheses
-ob = Suppress("[")	# open brackets
-cb = Suppress("]")	# close brackets
+varName = Combine(OneOrMore(Word(alphas)) + ZeroOrMore(Word(alphanums + "_"))) #example n camelCase n1 with_underscore
+
+c = Suppress(",")	#comma
+op = Suppress("(")	#open parentheses
+cp = Suppress(")")	#close parentheses
+osb = Suppress("[")	#open square brackets
+csb = Suppress("]")	#close square brackets
+ocb = Suppress("{")	#open curly brackets
+ccb = Suppress("}")	#close curly brackets
+
+length = Word(nums) | Combine(Word("{") + varName + Word("}"))
 
 #primitives
-primitiveType = op + type + cp	# example (int)
-primitiveNameType = op + type + c + variableName + cp	# example (str, name)
-primitiveNameTypeLen = op + type + c + variableName + c + Word(alphas) + cp	# example (str, names, 3)
-primitive = Group(primitiveType | primitiveNameType | primitiveNameTypeLen)
+primitiveType = op + (pType | sType) + cp	#example (int) (str)
+primitiveNameType = op + (pType | sType) + c + varName + cp	#example (int, name) (str, name)
+primitiveNameLen = op + sType + c + length + cp	#example (str, 3) (str, {n}) NOT (int, 3)
+primitiveNameTypeLen = op + sType + c + varName + c + length + cp	#example (bytes, names, 3)
+primitive = Group(primitiveType | primitiveNameType | primitiveNameLen | primitiveNameTypeLen) #one of the above
+
+#dictionaries
+dictionary = ocb + immutable + c + pType + ccb  #example {int, float}
 
 #containers
+containerSimple = osb + cType + c + length + c + primitive + csb    #example [list, 10, (int)]
+containerName = osb + cType + c + length + c + primitive + c + varName + csb    #example [list, 10, (int), name]
 
+container = Group(containerSimple | containerName)
 
 #grammar
 grammar = Forward()
-grammar << ZeroOrMore(primitive)
+grammar = (ZeroOrMore(primitive | container))
 
 #test
-toParse = "(int) (str, name) (str, 100) (bool, result_best)"
-res = grammar.parseString(toParse)
+toParse = """
+(int)
+(str, name)
+(bool, result_best)
+(str, 10)
+(bytes, {n})
+(bytearray, 3)
+(long)
 
-print(res)
+[list, 10, (str)]
+[list, {n}, (int), name]
+"""
+
+print(grammar.parseString(toParse))
