d1f53851f5d330b24b9e9ca575e109b007b61efb
msramalho <up201403027@fe.up.pt>
2017-10-07 22:24:37 +0100

    mutable vs immutable is working

--- pyhparser/grammar.py
+++ pyhparser/grammar.py
@@ -1,66 +1,80 @@
 from pyparsing import *
 
+#delimiters
+c = Suppress(",") #comma
+op = Suppress("(") #open parentheses
+cp = Suppress(")") #close parentheses
+osb = Suppress("[") #open square brackets
+csb = Suppress("]") #close square brackets
+ocb = Suppress("{") #open curly brackets
+ccb = Suppress("}") #close curly brackets
+
 #elementary operators
-pType = Or(Word("int") | Word("long") | Word("float") | Word("bool") | Word("complex")) #primitive types
-sType = Or(Word("str") | Word("bytes") | Word("bytearray")) #sequence types
-cType = Or(Word("list") | Word("dict") | Word("set") | Word("tuple") | Word("frozenset")) #containerTypes
-aType = Or(pType | sType | cType) #any type
+pType = Or([Literal("int"), Literal("long"), Literal("float"), Literal("bool"), Literal("complex")]) #primitive types
+sType = Or([Literal("str"), Literal("bytes")]) #sequence types
+ciType = Or([Literal("tuple"), Literal("frozenset")]) #container immutable ypes
+cmType = Or([Literal("list"), Literal("dict"), Literal("set"), Literal("bytearray")]) #container mutable types
 
-immutable = Or(pType | Word("str") | Word("bytes") | Word("tuple") | Word("frozenset"))
-
+#useful base constructs
 varName = Combine(OneOrMore(Word(alphas)) + ZeroOrMore(Word(alphanums + "_"))) #example n camelCase n1 with_underscore
-
-c = Suppress(",")	#comma
-op = Suppress("(")	#open parentheses
-cp = Suppress(")")	#close parentheses
-osb = Suppress("[")	#open square brackets
-csb = Suppress("]")	#close square brackets
-ocb = Suppress("{")	#open curly brackets
-ccb = Suppress("}")	#close curly brackets
-
-length = Or(Word(nums) | Combine(Word("{") + varName + Word("}")))
+length = Or([Word(nums), Combine(Literal("{") + varName + Literal("}"))]) #example 10 {varName}
 
 #primitives
-primitiveType = op + Or(pType | sType) + cp	#example (int) (str)
-primitiveNameType = op + Or(pType | sType) + c + varName + cp	#example (int, name) (str, name)
-primitiveNameLen = op + sType + c + length + cp	#example (str, 3) (str, {n}) NOT (int, 3)
-primitiveNameTypeLen = op + sType + c + varName + c + length + cp	#example (bytes, names, 3)
-primitive = Group(primitiveType | primitiveNameType | primitiveNameLen | primitiveNameTypeLen) #one of the above
+primitiveType = op + Or([pType, sType]) + cp #example (int) (str)
+primitiveNameType = op + Or([pType, sType]) + c + varName + cp #example (int, name) (str, name)
+primitiveNameLen = op + sType + c + length + cp #example (str, 3) (str, {n}) NOT (int, 3)
+primitiveNameTypeLen = op + sType + c + varName + c + length + cp #example (bytes, names, 3)
+primitive = Or([primitiveType, primitiveNameType, primitiveNameLen, primitiveNameTypeLen]) #one of the above
+primitive = Group(primitive)
 
-#containers
-containerSimple = osb + cType + c + length + c + primitive + csb    #example [list, 10, (int)]
-containerName = osb + cType + c + length + c + primitive + c + varName + csb    #example [list, 10, (int), name]
+#containers immutable
+containerISimple = osb + ciType + c + length + c + primitive + csb #example [frozenset, 10, (int)]
+containerIName = osb + ciType + c + length + c + primitive + c + varName + csb #example [tuple, 10, (int), name]
+containerI = Or([containerISimple, containerIName])
+containerI = Group(containerI)
 
-container = Group(containerSimple | containerName)
+#containers mutable
+containerMSimple = osb + cmType + c + length + c + primitive + csb #example [list, 10, (int)]
+containerMName = osb + cmType + c + length + c + primitive + c + varName + csb #example [list, 10, (int), name]
+containerM = Or([containerMSimple, containerMName])
+containerM = Group(containerM)
 
+#containers global
+container = Or([containerI, containerM])
 
 #dictionaries
-#TODO: decide how dictionaries will be, this will depend on immutable, but maybe the immutable have to be redifined on their own
-dictionary = ocb + immutable + c + aType + ccb  #example {int, float} {str, list}
+dictionary = ocb + Or([primitive, containerI]) + c + Or([primitive, container]) + ccb #example {int, float} {str, list}
+dictionary = Group(dictionary)
 
-
-#primitives or container = poc
-poc = ZeroOrMore(primitive | container)
 
 #grammar
 grammar = Forward()
-grammar = poc
+grammar = ZeroOrMore(primitive | container | dictionary)
 
 #test
-toParse = """
+toParsePrimitive = """
 (int)
+(long, name)
 (str, name)
 (bool, result_best)
 (str, 10)
 (bytes, {n})
-(bytearray, 3)
-(long)
-
-[list, 10, (str)]
-[list, {n}, (int), name]
-
-{(int), int}
-{int, str}
 """
 
+toParseContainer = """
+[list, 10, (str)]
+[list, {n}, (int), name]
+"""
+
+toParseDictionary = """
+{(int),(int)}
+{(int, lol),(int)}
+{(int, lol),[list, 10, (str, 2)]}
+"""
+
+toParse = toParsePrimitive + toParseContainer + toParseDictionary
 print(grammar.parseString(toParse))
+
+print(ZeroOrMore(primitive).parseString(toParsePrimitive))
+print(ZeroOrMore(container).parseString(toParseContainer))
+print(ZeroOrMore(dictionary).parseString(toParseDictionary))
