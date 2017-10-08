0fc534a079ff08810776aada60105b8b8269246c
msramalho <up201403027@fe.up.pt>
2017-10-08 23:36:38 +0100

    __initi__ fixes

--- pyhparser/__init__.py
+++ pyhparser/__init__.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append('../')
 from pyhparser.pyhparser import *
-from pyhparser.cleaner import *
+from pyhparser.grammar import *
 from pyhparser.utils import *
 from pyhparser.classParser import *
