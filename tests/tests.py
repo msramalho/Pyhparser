bfc2763afe26c97ba99a962488d6b75b761ff405
msramalho <up201403027@fe.up.pt>
2018-01-19 13:23:17 +0000

    travis coverage and more tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,12 +1,14 @@
 import unittest
 import sys
-sys.path.append("..")
+sys.path.append('../')
 from pyhparser import Pyhparser
+
 
 def getParseVars(inputs, parser, classes=[]):
     p = Pyhparser(inputs, parser, classes)
     p.parse()
     return p.getVariables()
+
 
 class PrimitivesTest(unittest.TestCase):
     def test_int(self):
@@ -16,7 +18,32 @@ class PrimitivesTest(unittest.TestCase):
             t = getParseVars(s, "(int)")
             self.assertDictEqual(t, {})
             t = getParseVars(s, "(int,n)")
-            self.assertDictEqual(t, {"n":i})
+            self.assertDictEqual(t, {"n": i})
+        with self.assertRaises(ValueError):
+            t = getParseVars("10.12", "(int)")
+
+
+class ContainersTest(unittest.TestCase):
+    def test_list(self):
+        l = [10, 20, 30, 40]
+        s = " ".join(str(x) for x in l)
+        t = getParseVars(s, "[list, %d, (int), l1]" % len(l))
+        self.assertDictEqual(t, {"l1": l})
+        with self.assertRaises(ValueError):
+            t = getParseVars("10.12", "(int)")
+
+
+class DictionaryTest(unittest.TestCase):
+    def test_list(self):
+        t = getParseVars("10 blue 20 green", "{(int), (str), myDict2, 2}")
+        self.assertDictEqual(t, {"myDict2": {10: "blue", 20: "green"}})
+        with self.assertRaises(ValueError):
+            t = getParseVars("10.12", "(int)")
+
+class ClassTest(unittest.TestCase):
+    def test_list(self):
+        t = getParseVars("10 blue 20 green", "{(int), (str), myDict2, 2}")
+        self.assertDictEqual(t, {"myDict2": {10: "blue", 20: "green"}})
         with self.assertRaises(ValueError):
             t = getParseVars("10.12", "(int)")
 
