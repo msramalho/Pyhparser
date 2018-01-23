eb4909cbf50cba4d391c874a1457aca768747d23
msramalho <up201403027@fe.up.pt>
2018-01-23 00:59:18 +0000

    more tests

--- tests/tests.py
+++ tests/tests.py
@@ -22,6 +22,15 @@ class PrimitivesTest(unittest.TestCase):
         with self.assertRaises(ValueError):
             t = getParseVars("10.12", "(int)")
 
+    def test_float(self):
+        floats = [-10, -3.2, 0, 1.4, 12]
+        for f in floats:
+            s = "%s" % f
+            t = getParseVars(s, "(float)")
+            self.assertDictEqual(t, {})
+            t = getParseVars(s, "(float,n)")
+            self.assertDictEqual(t, {"n": f})
+
 
 class ContainersTest(unittest.TestCase):
     def test_list(self):
@@ -34,18 +43,49 @@ class ContainersTest(unittest.TestCase):
 
 
 class DictionaryTest(unittest.TestCase):
-    def test_list(self):
+    def test_dict(self):
         t = getParseVars("10 blue 20 green", "{(int), (str), myDict2, 2}")
         self.assertDictEqual(t, {"myDict2": {10: "blue", 20: "green"}})
         with self.assertRaises(ValueError):
             t = getParseVars("10.12", "(int)")
 
+
 class ClassTest(unittest.TestCase):
-    def test_list(self):
-        t = getParseVars("10 blue 20 green", "{(int), (str), myDict2, 2}")
-        self.assertDictEqual(t, {"myDict2": {10: "blue", 20: "green"}})
-        with self.assertRaises(ValueError):
-            t = getParseVars("10.12", "(int)")
+    def test_class_incomplete(self):
+        t = getParseVars(
+            "10.23 55", "[class, Complex, {realpart: (float), imagpart: (int)}, cn1]", [Complex])
+        self.assertIsInstance(t["cn1"], Complex)
+        self.assertEqual(t["cn1"].realpart, 10.23)
+        self.assertEqual(t["cn1"].imagpart, 55)
+        self.assertEqual(t["cn1"].special, "not special")
+
+    def test_class_complete(self):
+        t = getParseVars("3.141592653 70 1300     veryNice",
+                         "[class, Complex, {realpart: (float), imagpart: (int), special: {(int), (str)}}, cn2]", [Complex])
+        self.assertIsInstance(t["cn2"], Complex)
+        self.assertEqual(t["cn2"].realpart, 3.141592653)
+        self.assertEqual(t["cn2"].imagpart, 70)
+        self.assertDictEqual(t["cn2"].special, {1300: "veryNice"})
+
+    def test_class_complete_inner(self):
+        t = getParseVars("3.141592653 70 4        string with four words",
+                         "[class, Complex, {realpart: (float), imagpart: (int), special: {(int, myInt), (str, {myInt})}}, cn3]", [Complex])
+        self.assertIsInstance(t["cn3"], Complex)
+        self.assertEqual(t["cn3"].realpart, 3.141592653)
+        self.assertEqual(t["cn3"].imagpart, 70)
+        self.assertDictEqual(t["cn3"].special, {4: "string with four words"})
+
+    def test_class_missing(self):  # class complex is unknown so it fails
+        with self.assertRaises(Exception):
+            t = getParseVars(
+                "10.23 55", "[class, Complex, {realpart: (float), imagpart: (int)}, cn4]")
+
+
+class Complex:
+    def __init__(self, realpart, imagpart, special="not special"):
+        self.realpart = realpart
+        self.imagpart = imagpart
+        self.special = special
 
 
 if __name__ == '__main__':
